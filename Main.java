c344662a99c1c82aab38cd6b763eebad3f057464
martinvalenciap <125387154+martinvalencia001@users.noreply.github.com>
2023-03-07 04:48:11 +0000

    The program should display a menu with five options and respond accordingly when the user makes a choice. The user should be able to view information on salary, job, and demand, as well as view a list of current students. The program should quit when the user chooses to exit.

--- Main.java
+++ Main.java
@@ -1,14 +1,54 @@
 import java.io.FileReader;
-import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Main {
-	public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws FileNotFoundException {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<String> students = new ArrayList<>();
+    FileReader fr = new FileReader("names.txt");
+    Scanner fileScanner = new Scanner(fr);
 
-		ArrayList<String> arrayList = new ArrayList<>();
-		FileReader fr = new FileReader("names.txt");
-		Scanner fileScanner = new Scanner(fr);
-		
-	}
+    while (fileScanner.hasNextLine()) {
+      String name = fileScanner.nextLine();
+      students.add(name);
+    }
+
+    boolean quit = false;
+    while (!quit) {
+      System.out.println("Press 1 to learn about salary.");
+      System.out.println("Press 2 to learn about the job.");
+      System.out.println("Press 3 to learn about demand.");
+      System.out.println("Press 4 to view current students.");
+      System.out.println("Press 5 to quit.");
+      int choice = scanner.nextInt();
+
+      switch (choice) {
+        case 1:
+          System.out.println("$98,345 average salary in South Florida!");
+          break;
+        case 2:
+          System.out.println("US News - 100 Best Jobs!");
+          break;
+        case 3:
+          System.out.println("Top 10 Forbes In-Demand Jobs!");
+          break;
+        case 4:
+          System.out.println("Current Students:");
+          for (String student : students) {
+            System.out.println(student);
+          }
+          break;
+        case 5:
+          quit = true;
+          break;
+        default:
+          System.out.println("Invalid choice. Please try again.");
+          break;
+      }
+    }
+
+    scanner.close();
+  }
 }
